7446a9a34bb12ab966045f79db10301855b51f0e
Tan-Junda <jtan5@uvm.edu>
2019-10-27 04:22:21 +0800

    first commit

---
+++ app/src/main/java/jtan5/example/musicplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package jtan5.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
